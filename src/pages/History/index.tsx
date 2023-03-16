0aa883770d9cd7e2c2fe25652edbd1d3dd94f487
Jean Victor <jeamsillva3@gmail.com>
2023-03-15 23:51:12 -0300

    Atualização

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,5 +1,56 @@
-import { HistoryContainer } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 import './styles.ts'
 export function History() {
-  return <HistoryContainer>Hello History</HistoryContainer>
+  return (
+    <HistoryContainer>
+      <h1> Histórico de tarefas </h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefas</td>
+              <td>20 minutos</td>
+              <td>Cerca de 2 semanas</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefas</td>
+              <td>20 minutos</td>
+              <td>Cerca de 2 semanas</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefas</td>
+              <td>20 minutos</td>
+              <td>Cerca de 2 semanas</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefas</td>
+              <td>20 minutos</td>
+              <td>Cerca de 2 semanas</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -79,7 +79,7 @@ export const Separator = styled.div`
   justify-content: center;
 `
 
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -93,9 +93,7 @@ export const StartCountdownButton = styled.button`
   font-weight: bold;
   cursor: pointer;
 
-  background: ${(props) => props.theme['green-500']};
   color: ${(props) => props.theme['gray-100']};
-
   transition: background 0.3s;
 
   &:disabled {
@@ -103,12 +101,24 @@ export const StartCountdownButton = styled.button`
     cursor: not-allowed;
   }
 
-  &:not(:disabled):hover {
-    background: ${(props) => props.theme['green-300']};
-  }
-
-  .bx-play {
+  .icon {
     font-size: 2rem;
     line-height: 0;
+  }
+`
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['green-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-300']};
+    transform: translateY(-0.2em);
+  }
+`
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['red-500']};
+
+  &:hover {
+    background: ${(props) => props.theme['red-700']};
+    transform: translateY(-0.2em);
   }
 `
