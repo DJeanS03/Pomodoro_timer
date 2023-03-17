d20edfa644a80157297cd8bebd24663468013331
Jean Victor <jeamsillva3@gmail.com>
2023-03-17 15:05:28 -0300

    New Update

--- src/pages/Home/Countdown/index.tsx
+++ src/pages/Home/Countdown/index.tsx
@@ -1,6 +1,58 @@
+import { differenceInSeconds } from 'date-fns'
+import { useContext, useEffect, useState } from 'react'
+import { CyclesContext } from '..'
 import { CountdownContainer, Separator } from './styles'
 
 export function Countdown() {
+  const { activeCycle, activeCycleId, markCurrentCycleAsFinished } =
+    useContext(CyclesContext)
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0) // o tando de segundos que se passaram após o ciclo ser ativo
+
+  useEffect(() => {
+    let interval: number
+
+    if (activeCycle) {
+      interval = setInterval(() => {
+        const secondsDifference = differenceInSeconds(
+          new Date(),
+          activeCycle.startDate,
+        )
+
+        if (secondsDifference >= totalSeconds) {
+          markCurrentCycleAsFinished()
+          setAmountSecondsPassed(totalSeconds)
+          clearInterval(interval)
+        } else {
+          setAmountSecondsPassed(secondsDifference) // só atualiza os segundos passados se não completar o total de segundos
+        }
+      }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [activeCycle, totalSeconds, activeCycleId, markCurrentCycleAsFinished])
+
+  //= ============================TIMER==================================
+
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+  const minutesAmount = Math.floor(currentSeconds / 60) // arredonda o numero para baixo
+  const secondsAmount = currentSeconds % 60
+  const minutes = String(minutesAmount).padStart(2, '0') // se ele não tiver 2 caracteres, adiciona 'zeros' na frente
+  const seconds = String(secondsAmount).padStart(2, '0')
+  //= ===================================================================
+
+  //= ============================PAGE TITLE==============================
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds} - Time to Focus`
+    } else {
+      document.title = 'Pomodoro Timer'
+    }
+  }, [minutes, seconds, activeCycle])
+  //= ====================================================================
+
   return (
     <CountdownContainer>
       <span>{minutes[0]}</span>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,25 +4,14 @@ import * as zod from 'zod'
 import { differenceInSeconds } from 'date-fns'
 
 import {
-  CountdownContainer,
-  FormContainer,
   HomeContainer,
-  MinutesAmountInput,
-  Separator,
   StartCountdownButton,
   StopCountdownButton,
-  TaskInput,
 } from './styles'
-import { useEffect, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import { NewCycleForm } from './NewCycleForm'
 import { Countdown } from './Countdown'
 
-const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, 'informe a tarefa'),
-  minutesAmount: zod.number().min(1, 'O Ciclo deve ser maior que 5 minutos'),
-})
-
-type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 interface Cycle {
   id: string
   task: string
@@ -32,63 +21,33 @@ interface Cycle {
   finishedDate?: Date
 }
 
+interface CyclesContextData {
+  activeCycle: Cycle | undefined
+  activeCycleId: string | null
+  markCurrentCycleAsFinished: () => void
+}
+
+export const CyclesContext = createContext({} as CyclesContextData)
+
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([])
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null) // vê ciclo ativo
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0) // o tando de segundos que se passaram após o ciclo ser ativo
 
-  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
-    resolver: zodResolver(newCycleFormValidationSchema),
-    defaultValues: {
-      task: '',
-      minutesAmount: 0,
-    },
-  })
-
-  //= ============================TIMER==================================
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId) // pecorre o vetor de ciclos e encontra um ciclo que seu id seja igual o id do ciclo ativo
-  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
-  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
-  const minutesAmount = Math.floor(currentSeconds / 60) // arredonda o numero para baixo
-  const secondsAmount = currentSeconds % 60
-  const minutes = String(minutesAmount).padStart(2, '0') // se ele não tiver 2 caracteres, adiciona 'zeros' na frente
-  const seconds = String(secondsAmount).padStart(2, '0')
-  //= ===================================================================
-
-  useEffect(() => {
-    let interval: number
-
-    if (activeCycle) {
-      interval = setInterval(() => {
-        const secondsDifference = differenceInSeconds(
-          new Date(),
-          activeCycle.startDate,
-        )
-
-        if (secondsDifference >= totalSeconds) {
-          setCycles((state) =>
-            state.map((cycle) => {
-              if (cycle.id === activeCycleId) {
-                return { ...cycle, finishedDate: new Date() }
-              } else {
-                return cycle
-              }
-            }),
-          )
-          setAmountSecondsPassed(totalSeconds)
-          clearInterval(interval)
+  function markCurrentCycleAsFinished() {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, finishedDate: new Date() }
         } else {
-          setAmountSecondsPassed(secondsDifference) // só atualiza os segundos passados se não completar o total de segundos
+          return cycle
         }
-      }, 1000)
-    }
+      }),
+    )
+  }
 
-    return () => {
-      clearInterval(interval)
-    }
-  }, [activeCycle, totalSeconds, activeCycleId])
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId) // pecorre o vetor de ciclos e encontra um ciclo que seu id seja igual o id do ciclo ativo
 
-  function handleCreateNewCycle(data: NewCycleFormData) {
+  /* function handleCreateNewCycle(data: NewCycleFormData) {
     const newCycle: Cycle = {
       id: String(new Date().getTime()), // o id vai ser a data em milesegundo
       task: data.task,
@@ -99,7 +58,7 @@ export function Home() {
     setActiveCycleId(newCycle.id)
     setAmountSecondsPassed(0) // reinicia a contagem após colocar um novo ciclo, caso já tenha um em andamento
     reset() // apaga os caracteres do input após o submit
-  }
+  } */
 
   function handleStopCycle() {
     setCycles((state) =>
@@ -114,27 +73,21 @@ export function Home() {
     setActiveCycleId(null)
   }
 
-  //= ============================PAGE TITLE==============================
-  useEffect(() => {
-    if (activeCycle) {
-      document.title = `${minutes}:${seconds} - Time to Focus`
-    } else {
-      document.title = 'Pomodoro Timer'
-    }
-  }, [minutes, seconds, activeCycle])
-  //= ====================================================================
-
   //= ============================DATA VALIDATION=========================
-  const task = watch('task')
+  /* const task = watch('task')
   const isMinutesAmount = watch('minutesAmount')
-  const isSubmitDisable = !task || !isMinutesAmount
+  const isSubmitDisable = !task || !isMinutesAmount */
   //= ====================================================================
 
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
-        <NewCycleForm />
-        <Countdown />
+      <form /* onSubmit={handleSubmit(handleCreateNewCycle)}  */action="">
+        <CyclesContext.Provider
+          value={{ activeCycle, activeCycleId, markCurrentCycleAsFinished }}
+        >
+          {/* <NewCycleForm /> */}
+          <Countdown />
+        </CyclesContext.Provider>
 
         {activeCycle ? (
           <StopCountdownButton onClick={handleStopCycle} type="button">
@@ -142,7 +95,7 @@ export function Home() {
             Interromper
           </StopCountdownButton>
         ) : (
-          <StartCountdownButton disabled={isSubmitDisable} type="submit">
+          <StartCountdownButton /* disabled={isSubmitDisable} */ type="submit">
             <i className="bx bx-play icon"></i>
             Começar
           </StartCountdownButton>
